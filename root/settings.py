755aa1e3ab39e8fb0d315526c12b8a8a366a1cdd
Duckenson jacquet <duckens244@hotmail.com>
2023-02-07 09:57:49 -0500

    Add bootstrap.min.css.map, make migrations

--- root/settings.py
+++ root/settings.py
@@ -84,8 +84,11 @@ DATABASES = {
     }
 }
 
-DATABASES['default']=dj_database_url.config(databaseurl)
-
+DATABASES['default'] = dj_database_url.config(
+    default=databaseurl,
+    conn_max_age=600,
+    conn_health_checks=True,
+)
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
